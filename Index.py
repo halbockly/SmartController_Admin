a32a9f731d5a3ad9907a73df0cda567c27e8e4d7
hmohri <hmohri@anvil.ne.jp>
2019-08-15 22:32:18 +0900

    Initial commit

---
+++ Index.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+from bottle import TEMPLATE_PATH, jinja2_template as template
+from bottle import run, route, static_file
+
+# index.pyが設置されているディレクトリの絶対パスを取得
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# テンプレートファイルを設置するディレクトリのパスを指定
+TEMPLATE_PATH.append(BASE_DIR + "/views")
+
+
+@route('/static/<_dir>/<_filename>')
+def server_static(_dir, _filename):
+    return static_file(_filename, root='./static/' + _dir + '/')
+
+
+@route('/Home')
+def index():
+    return template('views/Admin/Home')
+
+
+@route('/Buttons')
+def buttons():
+    return template('views/Admin/Buttons')
+
+
+@route('/Status')
+def status():
+    return template('views/Admin/Status')
+
+
+if __name__ == "__main__":
+    run(host="localhost", port=8080, debug=True, reloader=True)
