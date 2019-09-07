61c54da0c5bb3759d55ff6ebb97c43d7ecf306fe
hmohri <hmohri@anvil.ne.jp>
2019-09-07 18:02:07 +0900

    Edit Form

---
+++ static/js/buttons.js
@@ -0,0 +1,19 @@
+$(function(){
+    $(".edit_btn").on("click", function(){
+        // データ取得
+        $(".mchn_id").text($(this).parent().prevAll(".dt_col_1").text());
+        $(".ed_col_2").val($(this).parent().prevAll(".dt_col_2").text());
+        $(".ed_col_3").val($(this).parent().prevAll(".dt_col_3").text());
+        $(".ed_col_4").val($(this).parent().prevAll(".dt_col_4").text());
+
+        // 編集フォーム表示
+        $(".edit_box").addClass("open");
+        $(".edit_box").removeClass("hide");
+    });
+
+    $(".cancel_btn").on("click", function(){
+        // キャンセルボタン押下時、編集フォーム非表示
+        $(".edit_box").addClass("hide");
+        $(".edit_box").removeClass("open");
+    });
+});
---
+++ static/js/status.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".edit_btn").on("click", function(){
+        // データ取得
+        $(".mchn_id").text($(this).parent().prevAll(".dt_col_1").text());
+        $(".ed_col_2").val($(this).parent().prevAll(".dt_col_2").text());
+
+        // 編集フォーム表示
+        $(".edit_box").addClass("open");
+        $(".edit_box").removeClass("hide");
+    });
+
+    $(".cancel_btn").on("click", function(){
+        // キャンセルボタン押下時、編集フォーム非表示
+        $(".edit_box").addClass("hide");
+        $(".edit_box").removeClass("open");
+    });
+});
