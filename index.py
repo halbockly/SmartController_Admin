1b01c98fac0c4c66cd9f7ff960e0558dba9ddae8
hmohri <hmohri@anvil.ne.jp>
2020-01-12 00:18:39 +0900

    fixed data dir

--- index.py
+++ index.py
@@ -3,19 +3,9 @@ import json
 import os
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-# from bottle import TEMPLATE_PATH, jinja2_template as template, redirect
-# from bottle import run, route, static_file, request
 
-# index.pyが設置されているディレクトリの絶対パスを取得
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# テンプレートファイルを設置するディレクトリのパスを指定
-# TEMPLATE_PATH.append(BASE_DIR + "/views")
-
-#
-# @route('/static/<_dir>/<_filename>')
-# def server_static(_dir, _filename):
-#     return static_file(_filename, root='./static/' + _dir + '/')
-
+BASE_DIR = os.path.abspath('/')
+DATA_DIR = os.path.join(BASE_DIR, "home/pi/")
 
 @app.route('/')
 def index():
@@ -24,9 +14,10 @@ def index():
 
 @app.route('/Kaden')
 def buttons():
-    with open("data/kaden.json", "r", encoding='utf8') as f:
+    with open(DATA_DIR + "kaden.json", "r", encoding='utf8') as f:
         data = json.load(f)
     return render_template('Admin/Kaden.html', data=data)
+
 
 @app.route('/saveKaden', methods=["POST"])
 def save_button():
@@ -34,7 +25,7 @@ def save_button():
     status = request.form["status"]
     id = request.form["id"]
 
-    path = "data/kaden.json"
+    path = DATA_DIR + "kaden.json"
     with open(path, "r", encoding='utf8') as f:
         data = json.load(f)
         data[id]["name"] = name
@@ -43,5 +34,6 @@ def save_button():
             json.dump(data, outfile, indent=4, ensure_ascii=False)
     return redirect("/Kaden")
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run()
