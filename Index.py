1dde94a963787837932b1db1310c55292dc4a41f
hmohri <hmohri@anvil.ne.jp>
2020-01-11 22:29:37 +0900

    changed bottle to flask

--- Index.py
+++ Index.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
 import json
 import os
-from bottle import TEMPLATE_PATH, jinja2_template as template, redirect
-from bottle import run, route, static_file, request
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+# from bottle import TEMPLATE_PATH, jinja2_template as template, redirect
+# from bottle import run, route, static_file, request
 
 # index.pyが設置されているディレクトリの絶対パスを取得
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # テンプレートファイルを設置するディレクトリのパスを指定
-TEMPLATE_PATH.append(BASE_DIR + "/views")
+# TEMPLATE_PATH.append(BASE_DIR + "/views")
+
+#
+# @route('/static/<_dir>/<_filename>')
+# def server_static(_dir, _filename):
+#     return static_file(_filename, root='./static/' + _dir + '/')
 
 
-@route('/static/<_dir>/<_filename>')
-def server_static(_dir, _filename):
-    return static_file(_filename, root='./static/' + _dir + '/')
-
-
-@route('/')
+@app.route('/')
 def index():
-    return template('views/Admin/Home')
+    return render_template('Admin/Home.html')
 
 
-@route('/Kaden')
+@app.route('/Kaden')
 def buttons():
     with open("data/kaden.json", "r", encoding='utf8') as f:
         data = json.load(f)
-    return template('views/Admin/Kaden', data=data)
+    return render_template('Admin/Kaden.html', data=data)
 
-@route('/saveKaden', method="POST")
+@app.route('/saveKaden', methods=["POST"])
 def save_button():
-    name = request.forms.name
-    status = request.forms.status
-    id = request.forms.id
+    name = request.form['name']
+    status = request.form["status"]
+    id = request.form["id"]
 
     path = "data/kaden.json"
     with open(path, "r", encoding='utf8') as f:
@@ -39,7 +41,7 @@ def save_button():
         data[id]["status"] = status
         with open(path, 'w', encoding='utf8') as outfile:
             json.dump(data, outfile, indent=4, ensure_ascii=False)
-    redirect("/Kaden")
+    return redirect("/Kaden")
 
 if __name__ == "__main__":
-    run(host="localhost", port=8080, debug=True, reloader=True)
+    app.run(host="localhost", port=8080, debug=True)
